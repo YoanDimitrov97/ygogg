8a389c4b6933884d68ae84e35f0ee26d79936718
YoannFL <yoan.dimitrov97@gmail.com>
2024-10-25 16:40:05 +0300

    added custom format page

--- client/ygogg/src/App.tsx
+++ client/ygogg/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
+  type Card = {
+    id: number;
+    name: string;
+  };
+  const [data, setData] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true); // Loading state
+  const API_URL = "https://sheetdb.io/api/v1/8x87dvviqlcq8?sheet=Custom Format";
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data from SheetDB:", error);
+      } finally {
+        setLoading(false); // Set loading to false after fetching
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>OC Format</h1>
+      {!loading && (
+        <>
+          <h3>All Cards ({data.length})</h3>
+          <div className="allFormatCardsHolder">
+            {data.map((card) => (
+              <img
+                loading="lazy"
+                key={card.id}
+                src={`https://images.ygoprodeck.com/images/cards_small/${card.id}.jpg`}
+                alt={card.name}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
